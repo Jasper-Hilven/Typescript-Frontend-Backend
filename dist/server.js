ab55ade837ea9d8e170f34c16dd040df8c7917de
jasperh <jasper.hilven@xenit.eu>
2016-04-20 19:21:57 +0200

    Beautify server.js

--- dist/server.js
+++ dist/server.js
@@ -1,22 +1,22 @@
 var express = require('express'),
-http = require('http'),
-    path = require('path'),
-    keyValidatorModule = require('./../src/backend/chipinkeyvalidator.js'),
-    modelModule = require('./../src/backend/chipinmodel.js'),
-    providerModule = require('./../src/backend/chipinprovider.js'),
-    coreApiModule = require('./../src/backend/coreapi.js'),
-    restapiModule = require('./../src/backend/restapi.js');
+  http = require('http'),
+  path = require('path'),
+  keyValidatorModule = require('./../src/backend/chipinkeyvalidator.js'),
+  modelModule = require('./../src/backend/chipinmodel.js'),
+  providerModule = require('./../src/backend/chipinprovider.js'),
+  coreApiModule = require('./../src/backend/coreapi.js'),
+  restapiModule = require('./../src/backend/restapi.js');
 
 var chipinModel = modelModule();
 var keyValidator = keyValidatorModule();
 var provider = providerModule(chipinModel);
-var coreapi = coreApiModule(provider,keyValidator);
+var coreapi = coreApiModule(provider, keyValidator);
 var restapi = restapiModule(coreapi);
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
-app.use('/api',function(req, res, next) {
+app.use('/api', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -24,6 +24,6 @@ app.use('/api',function(req, res, next) {
 
 app.use('/api', restapi);
 app.use(express.static('dist'));
-http.createServer(app).listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
 });
