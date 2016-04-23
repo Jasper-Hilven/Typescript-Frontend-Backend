eed57555edbdc32442beacd1b4d33f31ac0cf1f3
Jasper Hilven <jasperhilven@gmail.com>
2016-04-23 10:16:16 +0200

    Add model check

--- src/backend/ChipinModel.ts
+++ src/backend/ChipinModel.ts
@@ -1,4 +1,7 @@
-export class User { }
+export class User {
+  constructor(public name: string, public email: string) {
+  }
+}
 export class Chipment {
   constructor(
     public author: User,
@@ -16,18 +19,55 @@ export class ChipmentForAuthor {
     public authorKey: string,
     public id: string) { }
 }
-export class Chipin { }
+export class Chipin {
+  constructor(public user: User, public amount: number) {
+
+  }
+}
 export class ChipinModelChecker {
+  private IsString(myVar) {
+    return (typeof myVar === 'string' || myVar instanceof String)
+  }
+  private IsNumber(myVar) {
+    return !isNaN(Number(myVar));
+  }
   IsChipment(data: any) {
-    throw "implement";
+    if (!data) return false;
+    var hasAuthor = this.IsUser(data.author);
+    var hasName = this.IsString(data.name);
+    var currency = this.IsString(data.currency);
+    var description = this.IsString(data.description);
+    var minContribution = this.IsNumber(data.maxContribution);
+    var maxContribution = this.IsNumber(data.minContribution);
+    return hasAuthor && hasName && currency &&
+      description && minContribution && maxContribution;
   }
   IsChipin(data: any) {
-    throw "implement";
+    return data && this.IsNumber(data.amount) && this.IsUser(data.user);
+  }
+  IsUser(data: any) {
+    return data && this.IsString(data.name) && this.IsString(data.email);
+  }
+  BuildUser(data: any) {
+    return new User(data.name, data.email);
   }
   BuildChipment(data: any): Chipment {
-    throw "implement";
+    var author = this.BuildUser(data.author);
+    var chipins = this.BuildChipins(data.chipin);
+    return new Chipment(author, data.name,
+      data.minContribution, data.maxContribution,
+      data.currency, data.description, chipins);
   }
   BuildChipin(data: any): Chipin {
-    throw "implement";
+    var user = this.BuildUser(data.user);
+    return new Chipin(user, data.amount);
+  }
+  BuildChipins(data: any): [Chipin] {
+    var ret: [Chipin] = <[Chipin]>[];
+    var arrayLength = data.length;
+    for (var i = 0; i < arrayLength; i++) {
+      ret.push(this.BuildChipin(data[i]));
+    }
+    return ret;
   }
 }
